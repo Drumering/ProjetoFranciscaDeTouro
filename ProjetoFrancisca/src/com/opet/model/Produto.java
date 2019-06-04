9e7048fbdee250581b041f5858fea69b2f85ac38
Anderson Mendes de Almeida <anderson.almeida@primecontrol.com.br>
2019-06-03 21:56:42 -0300

    update model

---
+++ ProjetoFrancisca/src/com/opet/model/Produto.java
@@ -0,0 +1,11 @@
+package com.opet.model;
+
+public class Produto {
+	private Categoria Cat;
+	private String proNome;
+	private double proAltura;
+	private double prolargura;
+	private double proCompr;
+	private double proPreco;
+	private int proQntd;
+}
